5a31a4ef50a465aec964dc64a3e8cb9913267acc
Lazir Pascual <lazirpascual@gmail.com>
2021-05-28 17:01:07 -0400

    and clear button and fix bug where calculator evaluates more than a single pair of numbers at a time

--- scripts/main.js
+++ scripts/main.js
@@ -3,13 +3,13 @@ const add = (numOne, numTwo) => numOne + numTwo;
 const subtract = (numOne, numTwo) => numOne - numTwo;
 const multiply = (numOne, numTwo) => numOne * numTwo;
 const divide = (numOne, numTwo) => numOne / numTwo;
-
 const operate = (numOne, numTwo, operator) => {
     return  (operator == "+") ? add(numOne, numTwo)
         :   (operator == "-") ? subtract(numOne, numTwo)
         :   (operator == "*") ? multiply(numOne, numTwo)
         :   divide(numOne, numTwo);
 }
+const roundToTwo = (num) => +(Math.round(num + "e+3")  + "e-3");
 
 const displayScreen = document.querySelector('#display');
 const mainButtons = document.querySelectorAll('button');
@@ -27,12 +27,25 @@ mainButtons.forEach((button) => {
     // display corresponding number for each click 
     button.addEventListener('click', () => {
         const result = document.createElement('h1');
+        
+        // check if current button are operators
         if (button.id == "+" || button.id == "-" || button.id == "*" || button.id == "/") {
-            numbers.operator = button.id;
-        } else if (button.id =="=") {
-            numbers.equals = button.id;
+            // only set the operator if it is empty
+            if (numbers.operator == "") {
+                numbers.operator = button.id;
+            }
+        } 
+
+        // all values filled, only need to calculate
+        if (numbers.first != "" && numbers.second != "" && numbers.operator != "")
+        {
+            // if the current button is an operator or an equal sign, calculate result
+            if (button.id == "+" || button.id == "-" || button.id == "*" || button.id == "/" || button.id == "=") {
+                numbers.equals = true;
+            } 
         }
 
+        // all situations that can occur when each button is pressed
         displayScreen.textContent = "";     
         if (numbers.first == "" && numbers.second == "" && numbers.operator == "" && numbers.equals == "") {
             numbers.first =  button.id;
@@ -44,16 +57,15 @@ mainButtons.forEach((button) => {
             displayScreen.appendChild(result);
         } else if (numbers.first != "" && numbers.second == "" && numbers.operator != "" && numbers.equals == "") {
             numbers.second =  "0";
-            result.textContent = button.id;     
+            result.textContent = numbers.first;     
             displayScreen.appendChild(result);
         } else if (numbers.first != "" && numbers.second != "" && numbers.operator != "" && numbers.equals == "") {
-            numbers.second = numbers.second + button.id;
+            numbers.second = parseInt(numbers.second) + button.id;
             result.textContent = numbers.second;     
             displayScreen.appendChild(result);
-        }
-        else if (numbers.first != "" && numbers.second != "" && numbers.operator != "" && numbers.equals != "") {
+        } else if (numbers.first != "" && numbers.second != "" && numbers.operator != "" && numbers.equals != "") {
             numbers.result = operate(Number(numbers.first), Number(numbers.second), numbers.operator);
-            result.textContent = numbers.result;     
+            result.textContent = roundToTwo(numbers.result);     
             displayScreen.appendChild(result);
             numbers.first =  "";
             numbers.second = "";
@@ -61,5 +73,14 @@ mainButtons.forEach((button) => {
             numbers.equals = "";
         }
 
+        // when "clear" button is pressed, empty everything and display 0
+        if (button.id == "clear") {
+            numbers.first =  "";
+            numbers.second = "";
+            numbers.operator = "";
+            numbers.equals = "";
+            result.textContent = "0";
+            displayScreen.appendChild(result);
+        }
     });
 });
